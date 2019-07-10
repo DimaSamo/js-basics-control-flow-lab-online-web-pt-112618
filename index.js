2380cf2fe95a482f66c4cbc8d0beef0b6ed7f6c1
Dmitri Samotessov <dimasamo96@gmail.com>
2019-07-10 05:46:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function scuberGreetingForFeet(distance){
 
 function ternaryCheckCity(city){
   // Write your code here!
-  city === "NYC" ? (return "Ok, sounds good.") : (return "No go.");
+  city === "NYC" ? ("Ok, sounds good.") : ("No go.");
 
 }
 
